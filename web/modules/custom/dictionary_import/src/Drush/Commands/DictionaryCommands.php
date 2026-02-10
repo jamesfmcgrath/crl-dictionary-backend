fe49428742446b8cb1aa28453ae5ef6eb27d1cb9
James McGrath <james@mcgrathmedia.com>
2026-02-10 19:22:48 +0000

    fix: Remove duplicated setup logic

--- web/modules/custom/dictionary_import/src/Drush/Commands/DictionaryCommands.php
+++ web/modules/custom/dictionary_import/src/Drush/Commands/DictionaryCommands.php
@@ -3,9 +3,6 @@
 namespace Drupal\dictionary_import\Drush\Commands;
 
 use Drupal\dictionary_import\Service\DictionaryImporter;
-use Drupal\field\Entity\FieldConfig;
-use Drupal\field\Entity\FieldStorageConfig;
-use Drupal\node\Entity\NodeType;
 use Drush\Commands\DrushCommands;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -61,76 +58,9 @@ class DictionaryCommands extends DrushCommands {
     $this->output()->writeln('Setting up Dictionary Entry content type and fields...');
 
     try {
-      // Create the Dictionary Entry content type.
-      if (!NodeType::load('dictionary_entry')) {
-        $type = NodeType::create([
-          'type' => 'dictionary_entry',
-          'name' => 'Dictionary Entry',
-        ]);
-        $type->save();
-        \Drupal::entityTypeManager()->clearCachedDefinitions();
-        $this->output()->writeln('<info>Created Dictionary Entry content type</info>');
-      }
-      else {
-        $this->output()->writeln('Dictionary Entry content type already exists');
-      }
-
-      // Create field_word storage (plain text, required on the bundle).
-      if (!FieldStorageConfig::loadByName('node', 'field_word')) {
-        $storage = FieldStorageConfig::create([
-          'field_name' => 'field_word',
-          'entity_type' => 'node',
-          'type' => 'string',
-          'settings' => [
-            'max_length' => 255,
-          ],
-          'cardinality' => 1,
-        ]);
-        $storage->save();
-        $this->output()->writeln('<info>Created field_word storage</info>');
-      }
-
-      if (!FieldConfig::loadByName('node', 'dictionary_entry', 'field_word')) {
-        $field = FieldConfig::create([
-          'field_name' => 'field_word',
-          'entity_type' => 'node',
-          'bundle' => 'dictionary_entry',
-          'label' => 'Word',
-          'required' => TRUE,
-        ]);
-        $field->save();
-        $this->output()->writeln('<info>Created field_word field</info>');
-      }
-      else {
-        $this->output()->writeln('field_word already exists');
-      }
-
-      // Create field_definitions storage (long text field).
-      if (!FieldStorageConfig::loadByName('node', 'field_definitions')) {
-        $storage = FieldStorageConfig::create([
-          'field_name' => 'field_definitions',
-          'entity_type' => 'node',
-          'type' => 'text_long',
-          'cardinality' => -1,
-        ]);
-        $storage->save();
-        $this->output()->writeln('<info>Created field_definitions storage</info>');
-      }
-
-      if (!FieldConfig::loadByName('node', 'dictionary_entry', 'field_definitions')) {
-        $field = FieldConfig::create([
-          'field_name' => 'field_definitions',
-          'entity_type' => 'node',
-          'bundle' => 'dictionary_entry',
-          'label' => 'Definitions',
-        ]);
-        $field->save();
-        $this->output()->writeln('<info>Created field_definitions field</info>');
-      }
-      else {
-        $this->output()->writeln('field_definitions already exists');
-      }
-
+      // Reuse the shared helper from the .install file
+      \Drupal::moduleHandler()->loadInclude('dictionary_import', 'install');
+      _dictionary_import_create_content_type_and_fields();
       $this->output()->writeln('<info>Setup complete!</info>');
     }
     catch (\Exception $e) {
